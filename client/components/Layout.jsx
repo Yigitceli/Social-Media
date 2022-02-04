0e2ebea8e4becb10dfee350a864494251e0334f7
Yigitceli <Yigit.celi@outlook.com>
2022-02-04 18:58:29 +0300

    navbar and searchbar config

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
+import { AiFillCloseCircle } from "react-icons/ai";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="flex h-screen flex-row w-100 ">
-      <Sidebar/>   
+    <div className="flex h-screen md:flex-row w-100 flex-col bg-mainColor">
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+      {isOpen && (
+        <div className="w-4/5 bg-white absolute md:hidden block animate-slide-in transition-all duration-200 ease-in-out">
+          <AiFillCloseCircle
+            onClick={() => setIsOpen(false)}
+            className="absolute top-5 right-5 text-3xl cursor-pointer"
+          />
+          <Sidebar />
+        </div>
+      )}
+      <Navbar setIsOpen={setIsOpen} />
+
       {children}
     </div>
   );
---
+++ client/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HiMenu } from "react-icons/hi";
+import { useSelector } from "react-redux";
+import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+
+export default function Navbar({ setIsOpen }) {
+  const { user } = useSelector((state) => state.user.data);
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex  justify-between shadow-md h-14 px-2 items-center md:hidden flex w-full">
+        <HiMenu
+          fontSize={40}
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer"
+        />
+        <img src="/logo.png" width={112} />
+        <img
+          src={user.photoURL}
+          className="rounded-full w-9 h-9 cursor-pointer"
+        />
+      </div>
+      <div className="my-5 px-4 flex w-full gap-2">
+        <div className="flex bg-white items-center gap-2 px-3 py-3 rounded-lg w-full">
+          <IoMdSearch fontSize={21}/>
+          <input type={"text"} placeholder="Search" className="outline-none w-full"/>
+        </div>
+        <img src={user.photoURL} className="cursor-pointer w-14 h-12 rounded-lg hidden md:block"/>
+        <div className="bg-black md:h-12 h-12 cursor-pointer flex w-12 md:w-14 items-center justify-center rounded-lg">
+          <IoMdAdd className="text-white" fontSize={20}/>
+        </div>
+      </div>
+    </div>
+  );
+}
